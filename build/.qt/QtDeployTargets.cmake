69cb04a1e2dd802e5adebfe7792ff965bccbfdd6
Alan W <awangc31s@gmail.com>
2026-04-20 11:33:54 -0400

    removed build and  cache

--- build/.qt/QtDeployTargets.cmake
+++
@@ -1,4 +0,0 @@
-set(__QT_DEPLOY_TARGET_simulation_FILE /home/alanxw/school/csci2240/env-induced-deformation/build/simulation)
-set(__QT_DEPLOY_TARGET_simulation_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_StaticGLEW_FILE /home/alanxw/school/csci2240/env-induced-deformation/build/libStaticGLEW.a)
-set(__QT_DEPLOY_TARGET_StaticGLEW_TYPE STATIC_LIBRARY)
